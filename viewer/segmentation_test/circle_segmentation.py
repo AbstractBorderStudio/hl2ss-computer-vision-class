271a506e5e6a488c494a59072cb9f94ffea77d1e
Daniel Bologna <89863727+AbstractBorderStudio@users.noreply.github.com>
2023-10-21 15:56:53 +0200

    clean code

--- viewer/segmentation_test/circle_segmentation.py
+++ viewer/segmentation_test/circle_segmentation.py
@@ -12,14 +12,14 @@ k = np.ones((5,5),np.float32)/25
 # grayscale image
 gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
 
-# Apply smoothing kernel to make gaussian blur
-# this reduce noise. Improves edge detection
+# Apply a smoothing kernel to make gaussian blur
+# this reduce noise. Improves edge detection.
 smoothed = cv2.GaussianBlur(gray, (3,3), cv2.BORDER_DEFAULT)
 
-# compute Canny edge detection for both normal and smoothed source
-# as we can see the smoothed one cathed only hard edged (circles)
-edges = cv2.Canny(gray,100,150,apertureSize = 3)
-edges_smoothed = cv2.Canny(smoothed,100,150,apertureSize = 3)
+# compute Canny edge detection for both normal and smoothed source.
+# As we can see the smoothed one cathes better circle's edges
+edges = cv2.Canny(gray, 100, 150, apertureSize=3)
+edges_smoothed = cv2.Canny(smoothed, 100, 150, apertureSize=3)
 
 # apply dialtion to highlight circlular areas.
 dilation = cv2.dilate(edges, np.ones((2,2)), iterations=1)
@@ -39,9 +39,8 @@ for circle in circles_smoothed[0,:]:
     x, y, r = circle
     center = (x,y)
     cv2.circle(smoothed, (int(x), int(y)), int(r), (255,0,0), cv2.FILLED)
-
-# canny_sobel = cv2.Canny(sobel_edges,100,110,apertureSize = 3)
  
+# ---------------------------- disply result ---------------------------- 
 plt.subplot(231), plt.imshow(edges)#, cmap="gray")
 plt.title("canny"), plt.axis("off")
 plt.subplot(234), plt.imshow(edges_smoothed)#, cmap="gray")
