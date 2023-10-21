d5fa2923a36e1a5abf7d6c3717544e5d050a7eee
Daniel Bologna <89863727+AbstractBorderStudio@users.noreply.github.com>
2023-10-21 15:52:47 +0200

    working circle detection

--- viewer/segmentation_test/circle_segmentation.py
+++ viewer/segmentation_test/circle_segmentation.py
@@ -11,14 +11,47 @@ k = np.ones((5,5),np.float32)/25
 
 # grayscale image
 gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
-canny_edges = cv2.Canny(gray,100,300,apertureSize = 3)
 
-x = cv2.Sobel(gray, cv2.CV_16S, 1, 0, ksize=3)
-y = cv2.Sobel(gray, cv2.CV_16S, 0, 1, ksize=3)
-sobel_edges = cv2.addWeighted(x, 0.5, y, 0.5, 0.0)
+# Apply smoothing kernel to make gaussian blur
+# this reduce noise. Improves edge detection
+smoothed = cv2.GaussianBlur(gray, (3,3), cv2.BORDER_DEFAULT)
 
-canny_sobel = cv2.Canny(sobel_edges,100,110,apertureSize = 3)
+# compute Canny edge detection for both normal and smoothed source
+# as we can see the smoothed one cathed only hard edged (circles)
+edges = cv2.Canny(gray,100,150,apertureSize = 3)
+edges_smoothed = cv2.Canny(smoothed,100,150,apertureSize = 3)
+
+# apply dialtion to highlight circlular areas.
+dilation = cv2.dilate(edges, np.ones((2,2)), iterations=1)
+dilation_smoothed = cv2.dilate(edges_smoothed, np.ones((2,2)), iterations=1)
+
+# apply hough transform to detect circles
+circles = cv2.HoughCircles(dilation, cv2.HOUGH_GRADIENT, dp=1, minDist=5, param1=150, param2=20, minRadius=10)
+circles_smoothed = cv2.HoughCircles(dilation_smoothed, cv2.HOUGH_GRADIENT, dp=1, minDist=5, param1=150, param2=20, minRadius=10)
+
+# add circles to images
+for circle in circles[0,:]:
+    x, y, r = circle
+    center = (x,y)
+    cv2.circle(gray, (int(x), int(y)), int(r), (255,0,0), cv2.FILLED)
+
+for circle in circles_smoothed[0,:]:
+    x, y, r = circle
+    center = (x,y)
+    cv2.circle(smoothed, (int(x), int(y)), int(r), (255,0,0), cv2.FILLED)
+
+# canny_sobel = cv2.Canny(sobel_edges,100,110,apertureSize = 3)
  
-plt.subplot(121), plt.imshow(canny_edges)
-plt.subplot(122), plt.imshow(sobel_edges, cmap="gray")
+plt.subplot(231), plt.imshow(edges)#, cmap="gray")
+plt.title("canny"), plt.axis("off")
+plt.subplot(234), plt.imshow(edges_smoothed)#, cmap="gray")
+plt.title("canny smoothed"), plt.axis("off")
+plt.subplot(232), plt.imshow(dilation)#, cmap="gray")
+plt.title("dilation"), plt.axis("off")
+plt.subplot(235), plt.imshow(dilation_smoothed)#, cmap="gray")
+plt.title("dilation smoothed"), plt.axis("off")
+plt.subplot(233), plt.imshow(gray)
+plt.title("detection"), plt.axis("off")
+plt.subplot(236), plt.imshow(smoothed)
+plt.title("detection smoothed"), plt.axis("off")
 plt.show()
--- viewer/segmentation_test/tempCodeRunnerFile.py
+++ viewer/segmentation_test/tempCodeRunnerFile.py
@@ -1,1 +1,5 @@
-1
+
+#     # draw the outer circle
+#     cv2.circle(gray,(i[0],i[1]),i[2],(0,255,0),2)
+#     # draw the center of the circle
+#     cv2.circle(gray,(i[0],i[1]),2,(0,0,255),3)
